ca175e57250a4422a32fdd499c705bf0a433b361
github username <github email address>
2018-05-29 19:51:24 -0400

    seed file

---
+++ db/seeds.rb
@@ -0,0 +1,29 @@
+require_relative '../lib/player.rb'
+require_relative '../lib/game.rb'
+require_relative '../lib/team.rb'
+
+#Cleveland Cavaliers
+Player.create(name: "LeBron James", position: "F", minutes: 40, points: 28, field_goal_perc: 45.0, three_pt_perc: 33.3, free_throw_perc: 66.7, rebounds: 15, assists: 8, steals: 0, blocks: 2, personal_fouls: 4)
+Player.create(name: "Kevin Love", position: "F", minutes: 34, points: 15, field_goal_perc: 30.8, three_pt_perc: 50.0, free_throw_perc: 100, rebounds: 21, assists: 1, steals: 0, blocks: 3, personal_fouls: 4)
+Player.create(name: 'Tristan Thompson', position: "C", minutes: 22, points: 0, field_goal_perc: 0, three_pt_perc: 0, free_throw_perc: 0, rebounds: 4, assists: 2, steals: 0, blocks: 0, personal_fouls: 2)
+Player.create(name: "J.R Smith", position: "G", minutes: 28, points: 3, field_goal_perc: 25.0, three_pt_perc: 50.0, free_throw_perc: 0.0, rebounds: 0, assists: 0, steals: 0, blocks: 0, personal_fouls: 1)
+Player.create(name: "Kyrie Irving", position: "G", minutes: 34, points: 24, field_goal_perc: 45.5, three_pt_perc: 75.0, free_throw_perc: 100, rebounds: 3, assists: 2, steals: 0, blocks: 1, personal_fouls: 3)
+Player.create(name: "Richard Jefferson", position: "F", minutes: 14, points: 9, field_goal_perc: 50.0, three_pt_perc: 0.0, free_throw_perc: 75.0, rebounds: 4, assists: 0, steals: 0, blocks: 0, personal_fouls: 5)
+Player.create(name: "Iman Shumpert", position: "G", minutes: 16, points: 5, field_goal_perc: 33.3, three_pt_perc: 33.3, free_throw_perc: 0.0, rebounds: 5, assists: 0, steals: 0, blocks: 0, personal_fouls: 1)
+Player.create(name: "Kyle Korver", position: "G", minutes: 19, points: 0, field_goal_perc: 0, three_pt_perc: 0, free_throw_perc: 0, rebounds: 4, assists: 1, steals: 0, blocks: 0, personal_fouls: 0)
+# Player.create(name:, position:, minutes:, points:, field_goal_perc:, three_pt_perc:, free_throw_perc:, rebounds:, assists:, steals:, blocks:, personal_fouls:)
+# Player.create(name:, position:, minutes:, points:, field_goal_perc:, three_pt_perc:, free_throw_perc:, rebounds:, assists:, steals:, blocks:, personal_fouls:)
+#Golden State Warriors
+Player.create(name: "Kevin Durant", position: "F", minutes: 37, points: 38, field_goal_perc: 53.3, three_pt_perc: 50.0, free_throw_perc: 87.5, rebounds: 9, assists: 8, steals: 0, blocks: 0, personal_fouls: 1)
+Player.create(name: "Draymond Green", position: "F", minutes: 36, points: 9, field_goal_perc: 25.0, three_pt_perc: 20.0, free_throw_perc: 66.7, rebounds: 11, assists: 2, steals: 2, blocks: 1, personal_fouls: 4)
+Player.create(name: "Zaza Pachulia", position: "C", minutes: 14, points: 8, field_goal_perc: 80.0, three_pt_perc: 0.0, free_throw_perc: 0.0, rebounds: 5, assists: 0, steals: 2, blocks: 0, personal_fouls: 3)
+Player.create(name: "Klay Thompson", position: "G", minutes: 36, points: 6, field_goal_perc: 18.8, three_pt_perc: 0.0, free_throw_perc: 0.0, rebounds: 4, assists: 4, steals: 1, blocks: 0, personal_fouls: 3)
+Player.create(name: "Stephen Curry", position: "G", minutes: 34, points: 28, field_goal_perc: 50, three_pt_perc: 54.5, free_throw_perc: 0.0, rebounds: 6, assists: 10, steals: 3, blocks: 0, personal_fouls: 3)
+# Player.create(name:, position:, minutes:, points:, field_goal_perc:, three_pt_perc:, free_throw_perc:, rebounds:, assists:, steals:, blocks:, personal_fouls:)
+# Player.create(name:, position:, minutes:, points:, field_goal_perc:, three_pt_perc:, free_throw_perc:, rebounds:, assists:, :steals:, :blocks:, :personal_fouls:)
+# Player.create(:name:, :position:, :minutes:, :points:, :field_goal_perc:, :three_pt_perc:, :free_throw_perc:, :rebounds:, :assists:, :steals:, :blocks:, :personal_fouls:)
+# Player.create(:name:, :position:, :minutes:, :points:, :field_goal_perc:, :three_pt_perc:, :free_throw_perc:, :rebounds:, :assists:, :steals:, :blocks:, :personal_fouls:)
+# Player.create(:name:, :position:, :minutes:, :points:, :field_goal_perc:, :three_pt_perc:, :free_throw_perc:, :rebounds:, :assists:, :steals:, :blocks:, :personal_fouls:)
+# Player.create(:name:, :position:, :minutes:, :points:, :field_goal_perc:, :three_pt_perc:, :free_throw_perc:, :rebounds:, :assists:, :steals:, :blocks:, :personal_fouls:)
+# Player.create(:name:, :position:, :minutes:, :points:, :field_goal_perc:, :three_pt_perc:, :free_throw_perc:, :rebounds:, :assists:, :steals:, :blocks:, :personal_fouls:)
+# Player.create(:name:, :position:, :minutes:, :points:, :field_goal_perc:, :three_pt_perc:, :free_throw_perc:, :rebounds:, :assists:, :steals:, :blocks:, :personal_fouls:)
